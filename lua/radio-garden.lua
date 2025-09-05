0a49d35f1e13906d2074d2940a544d6311b9f3f2
James <jamessundaram8@gmail.com>
2025-09-05 13:43:58 +0100

    can switch between stations without overlapping, kills old radio before switch

--- lua/radio-garden.lua
+++ lua/radio-garden.lua
@@ -1,5 +1,17 @@
 local M = {}
 
+M.playing = false
+M.radio_buf = nil
+
+function M.get_pid()
+	local lines = vim.api.nvim_buf_get_lines(M.radio_buf, -2, -1, false)
+	if lines[1] == nil then
+		return nil
+	end
+	print(lines[1])
+	return lines[1]
+end
+
 function Dump(o)
 	if type(o) == "table" then
 		local s = "{ "
@@ -15,7 +27,24 @@ function Dump(o)
 	end
 end
 
-function M.search_api(input)
+function M.check_radio_buf()
+	local buf_list = vim.api.nvim_list_bufs()
+	local my_buf = nil
+	for i in ipairs(buf_list) do
+		if vim.api.nvim_buf_get_name(buf_list[i]) == "radio_buf" then
+			my_buf = buf_list[i]
+			return
+		end
+	end
+	if my_buf == nil then
+		M.radio_buf = vim.api.nvim_create_buf(false, true) -- Create a scratch buffer
+		vim.api.nvim_buf_set_name(M.radio_buf, "radio_buf") -- Name the buffer
+	else
+		return
+	end
+end
+
+function M.search_api_and_play(input)
 	local url = (string.format("'https://radio.garden/api/search/secure?q=%s'", input))
 
 	-- print(url)
@@ -23,7 +52,10 @@ function M.search_api(input)
 		.. url
 		.. " -A \"Mozilla/5.0 (compatible;  MSIE 7.01; Windows NT 5.0)\" | jq '[.hits.hits[] | ._source | if .stream != null then {title:.title, country:.subtitle, stream_url: .stream} else empty end]'"
 	local result_handle = io.popen(curl_cmd)
-	local buffer = vim.api.nvim_create_buf(true, false)
+	-- local buffer = vim.api.nvim_create_buf(true, false)
+
+	-- if radio_buf doesn't exist, create one
+
 	-- print(result_handle)
 	if result_handle == nil then
 		vim.notify("Error executing search command.", vim.log.levels.ERROR)
@@ -54,11 +86,11 @@ function M.search_api(input)
 	-- print("You entered: " .. input)
 
 	local lua_json = vim.fn.json_decode(json_string)
-	for i, r in pairs(lua_json) do
-		print(i, r.title, r.stream_url)
-		vim.api.nvim_buf_set_lines(buffer, -1, -1, false, { r.title .. " - " .. r.stream_url })
-	end
-	--
+	-- for i, r in pairs(lua_json) do
+	-- 	print(i, r.title, r.stream_url)
+	-- 	-- vim.api.nvim_buf_set_lines(buffer, -1, -1, false, { r.title .. " - " .. r.stream_url })
+	-- end
+	-- --
 	local Picker = require("snacks.picker")
 
 	Picker.pick({
@@ -68,8 +100,16 @@ function M.search_api(input)
 		end,
 		confirm = function(picker, item)
 			if item then
-				local MPV = vim.system({ "mpv", item.stream_url, "--loop-playlist=force" }, { stdin = true })
+				if M.get_pid() ~= nil then
+					print(M.get_pid())
+					-- M.radio_process:kill("SIGKILL")
+					vim.system({ "kill", M.get_pid() }, { stdin = true })
+				end
+				local radio_process = vim.system({ "mpv", item.stream_url, "--loop-playlist=force" }, { stdin = true })
+				vim.api.nvim_buf_set_lines(M.radio_buf, -2, -1, false, { tostring(radio_process.pid) })
+
 				vim.notify("Tuning in to " .. item.title .. " - " .. item.stream_url)
+				print(radio_process.pid)
 			else
 				vim.notify("No item selected", vim.log.levels.WARN)
 			end
@@ -84,7 +124,7 @@ function M.search_radio()
 	Snacks.input({
 		prompt = "Search Radio Garden Station Names: ",
 	}, function(input)
-		M.search_api(input)
+		M.search_api_and_play(input)
 	end)
 end
 
@@ -93,6 +133,7 @@ function M.setup(opts)
 	-- Merge user options with defaults
 	opts = opts or {}
 
+	M.check_radio_buf()
 	-- Create the user command
 	vim.api.nvim_create_user_command("MPV", function()
 		local stream_url = STREAM_URLS[1]
@@ -107,5 +148,14 @@ function M.setup(opts)
 		desc = "Say hello from our plugin",
 		silent = true, -- Prevents the command from being echoed in the command line
 	})
+
+	vim.keymap.set("n", "<leader>rp", function()
+		if M.radio_process == nil then
+			vim.notify("No radio is playing", vim.log.levels.WARN)
+			return
+		else
+			M.radio_process:write({ "p" })
+		end
+	end)
 end
 return M
